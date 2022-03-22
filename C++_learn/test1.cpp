971def6384971c54f8a9b0f3c3c94704da992271
lemtop1 <tf_sc_lem@foxmail.com>
2022-03-22 11:38:10 +0800

    the C++ enviroment is not work

---
+++ C++_learn/test1.cpp
@@ -0,0 +1,11 @@
+/* 1-1 .cpp*/
+#include<iostream.h>
+using namespace std;
+
+void main()
+{
+    int stu_number;
+    cout << "please input student number:";
+    cin << stu_number;
+    cout << "Hello!" << stu_number <<endl;
+}
--- learngit/readme.txt
+++
@@ -1,4 +0,0 @@
-Git is a disstributed version control system.
-GIt is free software distributed under the GPL.
-Git has a mutable index called stage.
-Git tracks changes.
